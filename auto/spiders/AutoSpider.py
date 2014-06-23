b0366683df5f753e47a0d8028dd3b82461f659f1
dzqlx1993 <dzqlx1993@gmail.com>
2014-06-23 10:52:34 +0800

    fixed selector

--- auto/spiders/AutoSpider.py
+++ auto/spiders/AutoSpider.py
@@ -14,9 +14,10 @@ class AutoSpider(Spider):
     
     def parse(self,response):
         sel = Selector(response)
-        brandSites = sel.xpath('//div[@class]/span[@class]')
+        brandSites = sel.xpath("//div[@class]/span[@class]")
         brandUrls = []
         for brandSite in brandSites:
+            print brandSite
             brandUrl = brandSite.xpath('a[@href]/@href').extract()
             try:
                 url = "http://car.autohome.com.cn" + brandUrl[0]
@@ -24,28 +25,33 @@ class AutoSpider(Spider):
                 brandUrls.append(url)
             except Exception, e:
                 pass
-        
         for carListUrl in brandUrls:
             yield Request(carListUrl,callback=self.parseBrand)
             
 
     def parseBrand(self,response):
         sel = Selector(response)
-        carLists = sel.xpath("//div[contains(@class='list-cont',@data-value)]")
+        carLists = sel.xpath("//div[@class='list-cont-main']")
         carBrand = sel.xpath("//h1[@class='seo-text']/text()").extract()
         print carBrand[0].encode('utf-8')
         items = []
         for car in carLists:
-            carType = car.xpath("//div[@class='main-title']/a/text()").extract()
-            carLevel = car.xpath("//div/ul[@class='lever-ul']/li[not(a|div|span)]/text()").extract()
-            carStructure = car.xpath("//div/ul[@class='lever-ul']/li[not(div|span)]/a/text()").extract()
-            carGearbox = car.xpath("//div/ul[@class='lever-ul']/li[4]/a/text()").extract()
-            carEngine = car.xpath("//div/ul[@class='lever-ul']/li[not(@class|div|a)]/span[@title]/a[@href]/text()").extract()
-            carPrice = car.xpath("//span[@class]/span[@class='font-arial']/text()").extract()
-            engineInfo = ''
-            for engine in carEngine:
-                engineInfo = engineInfo + " " + engine.encode('utf-8')
-            # carGearbox = info.xpath("a/text()").extract()
+            carType = car.xpath("div[@class='main-title']/a/text()").extract()
+            carLevel = car.xpath("div[@class='main-lever']/div/ul[@class='lever-ul']/li[not(a|div|span)]/text()").extract()
+            carStructure = car.xpath("div[@class='main-lever']/div/ul[@class='lever-ul']/li[not(div|span)]/a/text()").extract()
+            carGearboxList = car.xpath("div[@class='main-lever']/div/ul[@class='lever-ul']/li[4]/a")
+            carGearbox = ""
+            for gear in carGearboxList:
+                gearbox = gear.xpath("text()").extract()
+                carGearbox = carGearbox + " " + gearbox[0].encode('utf-8')
+
+            carEngineList = car.xpath("div[@class='main-lever']/div/ul[@class='lever-ul']/li[not(@class|div|a)]/span[@title]/a[@href]")
+            carPrice = car.xpath("div/div/div/span[@class]/span[@class='font-arial']/text()").extract()
+            carEngine = ""
+            for engine in carEngineList:
+                engineInfo = engine.xpath("text()").extract()
+                carEngine = carEngine + " " + engineInfo[0].encode('utf-8')
+
             try:
                 item = AutoItem()
                 item['brand'] = carBrand[0].encode('utf-8')
@@ -53,30 +59,28 @@ class AutoSpider(Spider):
                 item['level'] = carLevel[0].encode('utf-8')
                 item['structure'] = carStructure[0].encode('utf-8')
                 item['price'] = carPrice[0].encode('utf-8')
-                item['engine'] = engineInfo
-                item['gearbox'] = carGearbox[0].encode('utf-8')
+                item['engine'] = carEngine
+                item['gearbox'] = carGearbox
                 items.append(item)
             except Exception, e:
                 print e
-            return items
         
-        #如果有下一页的继续抓取下一页
-        carNextPageUrls = car.xpath("//div[@class='page']/a[not(@class)]/@href").extract()  
-
-        for pageUrl in carNextPageUrls:
-            nextPageUrl = "http://car.autohome.com.cn" + pageUrl
-            self.make_requests_from_url(nextPageUrl).replace(callback=self.parseNextPage)
+        return items
         
-    def parseNextPage(self,response):
-        sel = Selector(response)
-        carLists = sel.xpath("//div[contains(@class='list-cont',@data-value)]")
-        print "parseNextPage"
-        for car in carLists:
-            carType = car.xpath("div[@class='main-title']/a/text()").extract()
-            print "carlists"
-            try:
-                print carType[0].encode('utf-8')
-                print "running!!!!"
-            except Exception, e:
-                pass
-    
+    #     carNextPageUrls = sel.xpath("//div[@class='page']/a[not(@class)]/@href").extract() 
+    #     解析下一页的数据
+    #     for pageUrl in carNextPageUrls:
+    #         nextPageUrl = "http://car.autohome.com.cn" + pageUrl
+    #         self.make_requests_from_url(nextPageUrl).replace(callback=self.parseNextPage)
+        
+    # def parseNextPage(self,response):
+    #     print 'testtesttest'
+    #     sel = Selector(response)
+    #     carLists = sel.xpath("//div[contains(@class='list-cont',@data-value)]")
+    #     print "parseNextPage"
+    #         carType = car.xpath("div[@class='main-title']/a/text()").extract()
+    #         try:
+    #             print carType[0].encode('utf-8')
+    #         except Exception, e:
+    #             pass
+    # 
